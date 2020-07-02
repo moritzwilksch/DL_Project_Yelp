1081da90f9e405bb92a43c2099011e33569db506
Moritz Wilksch <moritzwilksch@gmail.com>
2020-07-02 08:02:40 +0200

    refactored data loading to functions

--- data_loading.py
+++ data_loading.py
@@ -2,19 +2,26 @@
 import pandas as pd
 from typing import List
 
-df: pd.DataFrame = pd.read_json('~/Downloads/yelp_dataset/yelp_academic_dataset_review.json', lines=True, chunksize=8192)
+
+def prepare_raw_data(path_to_json: str = None) -> pd.DataFrame:
+    """
+    Loads raw json file of reviews and preprocesses it using the prep function.
+    CAUTION: Long running time!
+    """
+    if path_to_json is None:
+        path_to_json = "~/Downloads/yelp_dataset/yelp_academic_dataset_review.json"
+
+    df: pd.DataFrame = pd.read_json(path_to_json, lines=True, chunksize=8192)
+    preped_chunks: List[pd.DataFrame] = []
+
+    for chunk in df:
+        preped_chunks.append(prep(chunk))
+
+    return pd.concat(preped_chunks)
 
 
 def prep(x: pd.DataFrame) -> pd.DataFrame:
+    """Drops all columns except for stars, text and date and optimizes datatypes."""
     x['text'] = x['text'].astype('string')
     x['stars'] = x['stars'].astype('category')
     return x[['stars', 'text', 'date']]
-
-
-preped_chunks: List[pd.DataFrame] = []
-
-for chunk in df:
-    preped_chunks.append(prep(chunk))
-
-#%%
-final = pd.concat(preped_chunks)
